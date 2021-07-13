06a2c31e571edbfd294303fa6d078396d069e1b9
Max Iefymenko <maksim82@hotmail.com>
2021-07-13 10:26:15 +0300

    Update. Readme.md

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Search from "../games/Search.js";
 import Games from "../games/Games.js";
+import Navbar from "./NavBar.js";
 
 const Index = () => {
   return (
     <>
+      <Navbar />
       <Search />
       <Games />
     </>
